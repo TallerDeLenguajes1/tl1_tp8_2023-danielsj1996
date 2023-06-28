27c2494593ac6b9ac32b4af779a8f1073096822a
“Daniel <danieljuarez1996@gmail.com>
2023-06-28 16:16:04 -0300

    creacion de archivo .csv en rama punto2

--- Program.cs
+++ Program.cs
@@ -1,154 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 
 
-Console.WriteLine("Indique cuantas tareas desea realizar: ");
 
-int cantidad = Convert.ToInt32(Console.ReadLine());
+List<string> archivo = Directory.GetFiles(@"C:\tl1_tp8_2023-danielsj1996").ToList();
 
-var realizadas = new List<Tarea>();
-
-var pendientes = new List<Tarea>();
-
-for (int i = 0; i < cantidad; i++)
+foreach (string archivos in archivo)
 {
-    Console.WriteLine("Tarea #" + (i + 1) + "\n");
-    CargarTarea(pendientes, i);
+    string nombreArchivo = Path.GetFileName(archivos);
+    string extension = Path.GetExtension(archivos);
+
+    Console.WriteLine($"Nombre: {nombreArchivo}, Extension: {extension}");
 }
-
-Console.WriteLine("\n");
-// mostrar Lista de Tareas
-
-Console.WriteLine("\nLos Tareas asignadas son:\n");
-mostrarTareas(pendientes);
-
-Console.WriteLine("\n");
-PendientesORealizadas(pendientes, realizadas);
-
-Console.WriteLine("\nLos Tareas realizadas son:\n");
-mostrarTareas(pendientes);
-
-Console.WriteLine("\nLos Tareas aun pendientes son:\n");
-mostrarTareas(pendientes);
+string csvRuta = Path.Combine(@"C:\tl1_tp8_2023-danielsj1996", "index.csv");
+GuardarArchivosCSV(archivo, csvRuta);
 
 
-
-string fileName = "HorasTrabajadas.txt";
-string mensaje = "El Total de horas trabajadas son: ";
-int Horaspend = Horas(pendientes);
-int Horasrealiz = Horas(realizadas);
-int HorasTotales = Horaspend + Horasrealiz;
+Console.WriteLine($"El listado de archivos se ha guardado en el archivo '{csvRuta}'.");
 
 
-FileStream fs = new FileStream(fileName, FileMode.Create);
-using (StreamWriter writer = new StreamWriter(fs))
+static void GuardarArchivosCSV(List<string> files, string csvRuta)
 {
-    writer.Write(mensaje);
-    writer.Write(HorasTotales);
-}
-
-// Create an instance of StreamReader to read from a file.
-// The using statement also closes the StreamReader.
-try
-{
-
-
-    using (StreamReader sr = new StreamReader("HorasTrabajadas.txt"))
+    using (StreamWriter writer = new StreamWriter(csvRuta))
     {
-        string? line;
-        // Read and display lines from the file until the end of
-        // the file is reached.
-        while ((line = sr.ReadLine()) != null)
+        writer.WriteLine("Indice"+" "+"Nombre"+" "+"Extension");
+
+        for (int i = 0; i < files.Count; i++)
         {
-            Console.WriteLine(line);
+            string nombreArchivo = Path.GetFileName(files[i]);
+            string extension = Path.GetExtension(files[i]);
+
+            writer.WriteLine($"{i + 1} -- {nombreArchivo} -- {extension}");
         }
     }
 }
-catch (Exception e)
-{
-    // Let the user know what went wrong.
-    Console.WriteLine("El archivo no se pudo leer:");
-    Console.WriteLine(e.Message);
-}
-
-//METODOS(FUNCIONES
-void CargarTarea(List<Tarea> pendiente, int cantidad)
-{
-    Random Rand = new Random();
-    string[] Tareas = { "Tarea1", "Tarea2", "Tarea3", "Tarea4", "Tarea5", "Tarea6" };
-    int T = Rand.Next(Tareas.Length);
-    var Tarea1 = new Tarea();
-    Console.WriteLine("Ingrese el ID de la tarea");
-    Tarea1.Id = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Ingrese la descripcion de la tarea");
-    Tarea1.Descripcion = Tareas[T];
-    Tarea1.Duracion = Rand.Next(1, 101);
-    pendiente.Add(Tarea1);
-}
-
-
-
-
-
-void PendientesORealizadas(List<Tarea> pendiente, List<Tarea> realizadas)
-{
-    int numTrealizadas = 0;
-    foreach (Tarea tarea in pendientes)
-    {
-        Console.WriteLine("Se realizo la tarea # ? (Si(1)/No(0))");
-        int respuesta = Convert.ToInt32(Console.ReadLine());
-        if (respuesta == 1)
-        {
-            realizadas.Add(tarea);
-        }
-        numTrealizadas++;
-    }
-    foreach (Tarea tarea in realizadas)
-    {
-        pendiente.Remove(tarea);
-    }
-
-
-}
-
-void mostrarTareas(List<Tarea> Listas)
-{
-
-    foreach (Tarea tarea in Listas)
-    {
-        Console.WriteLine($"ID :{tarea.Id} \n");
-        Console.WriteLine($"Descripcion : {tarea.Descripcion}\n");
-        Console.WriteLine($"Duracion : {tarea.Duracion}\n");
-
-    }
-}
-
-int Horas(List<Tarea> Listas)
-{
-    int HorasTotales = 0;
-    foreach (Tarea tarea in Listas)
-    {
-        HorasTotales += tarea.Duracion;
-
-    }
-    return HorasTotales;
-
-}
-
-
-
-
-
-public class Tarea
-{
-    private int id;
-    private string? descripcion;
-    private int duracion;
-    public int Id { get => id; set => id = value; }
-    public string? Descripcion { get => descripcion; set => descripcion = value; }
-    public int Duracion { get => duracion; set => duracion = value; }
-}
-
-
-
